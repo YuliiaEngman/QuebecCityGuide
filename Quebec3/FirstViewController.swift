e201662573a226d8d42ca51b8990d8d3f3f27482
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-04 15:34:25 -0500

    Initial Commit

---
+++ Quebec3/FirstViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Quebec3
+//
+//  Created by Yuliia Engman on 1/4/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var startButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
