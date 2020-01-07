58d62b2aae4b145e37ea9b18d505730802eb3ca0
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-07 14:02:00 -0500

    added more code to CreperiesVS

--- Quebec3/CreperiesViewController.swift
+++ Quebec3/CreperiesViewController.swift
@@ -11,13 +11,89 @@ import UIKit
 class CreperiesViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+      //  loadData()
     }
+    
+//    func loadData() {
+//            sightSeeings = FreeSightseeing.freeSightseeing
+//           }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let detailCafeVC = segue.destination as? CreperiesDetailViewController, let indexPath = tableView.indexPathForSelectedRow else {
+            fatalError("failed to get indexPath and detailCafeVC")
+        }
+        
+//        let creperie =
+//        let sightseeing = sightSeeings[indexPath.row]
+        //        detailVC.freesightseeing = sightseeing
+    }
 }
+
+extension CreperiesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FoodCell", for: indexPath)
+        
+       // let sightSeeing = sightSeeings[indexPath.row]
+      //  cell.textLabel?.text = sightSeeing.title
+        
+        return cell
+    }
+}
+
+extension CreperiesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+}
+
+//@IBOutlet weak var tableView: UITableView!
+//
+//    var sightSeeings = [FreeSightseeing]() {
+//        didSet {
+//            tableView.reloadData()
+//        }
+//    }
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        tableView.dataSource = self
+//        loadData()
+//    }
+//
+//    func loadData() {
+//        sightSeeings = FreeSightseeing.freeSightseeing
+//       }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        guard let detailVC = segue.destination as? FreeDetailViewController, let indexPath = tableView.indexPathForSelectedRow else {
+//            fatalError("failed to get indexPath and detailVC")
+//        }
+//        let sightseeing = sightSeeings[indexPath.row]
+//        detailVC.freesightseeing = sightseeing
+//    }
+//
+//}
+//
+//extension FreeSightseeingViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return sightSeeings.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "FreeCell", for: indexPath)
+//
+//        let sightSeeing = sightSeeings[indexPath.row]
+//        cell.textLabel?.text = sightSeeing.title
+//
+//        return cell
+//    }
+//}
+
