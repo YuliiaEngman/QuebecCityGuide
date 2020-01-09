724671cc7ed34d5cc951110f2b398338bbce1b76
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-09 17:50:01 -0500

    updated luxury VC

--- Quebec3/LuxeryViewController.swift
+++ Quebec3/LuxeryViewController.swift
@@ -8,23 +8,55 @@
 
 import UIKit
 
-class LuxeryViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+class LuxuryViewController: UIViewController {
     
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var luxurySightSeeings = [LuxurySightseeing]() {
+            didSet {
+                tableView.reloadData()
+            }
+        }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            tableView.dataSource = self
+            loadData()
+        }
+        
+        func loadData() {
+            luxurySightSeeings = LuxurySightseeing.luxurySightseeing
+           }
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            guard let detailVC = segue.destination as? FreeDetailViewController, let indexPath = tableView.indexPathForSelectedRow else {
+                fatalError("failed to get indexPath and detailVC")
+            }
+            let sightseeing = luxurySightSeeings[indexPath.row]
+            detailVC.luxurySightseeing = sightseeing
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+
+    extension LuxuryViewController: UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return luxurySightSeeings.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LuxuryCell", for: indexPath)
+            
+            let luxurySightSeeing = luxurySightSeeings[indexPath.row]
+            cell.textLabel?.text = luxurySightSeeing.title
+            
+            return cell
+        }
+    }
+
+    extension LuxuryViewController: UITableViewDelegate {
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 140
+        }
 
 }
