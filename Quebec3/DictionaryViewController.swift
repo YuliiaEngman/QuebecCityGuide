1fe6eb5ec868a3113f5877d436348ce70db8abe1
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-10 07:46:42 -0500

    dictionary VC populated with French words

--- Quebec3/DictionaryViewController.swift
+++ Quebec3/DictionaryViewController.swift
@@ -11,6 +11,8 @@ import AVFoundation
 
 class DictionaryViewController: UIViewController, AVAudioPlayerDelegate {
     
+    
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var bonjourButton: UIButton!
     
     
@@ -20,37 +22,157 @@ class DictionaryViewController: UIViewController, AVAudioPlayerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "BASIC FRENCH DICTIONARY"
+        textView.text = "Even though almost everyone speaks English as their second language in Quebec City it is still very rewording learning a few basic French words - believe me it will make a big difference to your trip and you will be more than welcome everywhere you come and to everybody you talk to. Here a few useful words and phrases in French with pronauncation."
         objPlayer?.delegate = self
     }
     
-  func playAudioFile() {
-        guard let url = Bundle.main.url(forResource: "Bonj", withExtension: "mp3") else {
-            print("can't find resource")
-            return
-    }
-
-        do {
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            // For iOS 11
-            objPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.m4a.rawValue)
-
-            // For iOS versions < 11
-           // objPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3)
-
-            guard let aPlayer = objPlayer else { return }
-            aPlayer.play()
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
+   // var url = Bundle.main.url(forResource: "Bonjour", withExtension: "mp3")
+    
+//  func playAudioFile() {
+//        guard let url = Bundle.main.url(forResource: "Bonjour", withExtension: "mp3") else {
+//            print("can't find resource")
+//            return
+//    }
+//        do {
+//            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//
+//            objPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.m4a.rawValue)
+//
+//            guard let aPlayer = objPlayer else { return }
+//            aPlayer.play()
+//
+//        } catch let error {
+//            print(error.localizedDescription)
+//        }
+//    }
 
     @IBAction func bonlourActionButton(_ sender: UIButton) {
-        playAudioFile()
-     
+        //playAudioFile()
+        
+        guard let url = Bundle.main.url(forResource: "Bonjour", withExtension: "mp3") else {
+                 print("can't find resource")
+                 return
+         }
+             do {
+                 try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+                 try AVAudioSession.sharedInstance().setActive(true)
+
+                 objPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.m4a.rawValue)
+
+                 guard let aPlayer = objPlayer else { return }
+                 aPlayer.play()
+
+             } catch let error {
+                 print(error.localizedDescription)
+             }
     }
+    
+    @IBAction func silVuPleActionButton(_ sender: UIButton) {
+//         var url = Bundle.main.url(forResource: "SilVuPle", withExtension: "mp3")
+//        playAudioFile()
+        
+        guard let url = Bundle.main.url(forResource: "SilVuPle", withExtension: "mp3") else {
+                print("can't find resource")
+                return
+        }
+            do {
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+                try AVAudioSession.sharedInstance().setActive(true)
+
+                objPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.m4a.rawValue)
+
+                guard let aPlayer = objPlayer else { return }
+                aPlayer.play()
+
+            } catch let error {
+                print(error.localizedDescription)
+            }
+    }
+    
+    @IBAction func ouiActionButton(_ sender: UIButton) {
+        guard let url = Bundle.main.url(forResource: "Oui", withExtension: "mp3") else {
+                print("can't find resource")
+                return
+        }
+            do {
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+                try AVAudioSession.sharedInstance().setActive(true)
+
+                objPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.m4a.rawValue)
+
+                guard let aPlayer = objPlayer else { return }
+                aPlayer.play()
+
+            } catch let error {
+                print(error.localizedDescription)
+            }
+    }
+    
+    @IBAction func nonActionButton(_ sender: UIButton) {
+        guard let url = Bundle.main.url(forResource: "Non", withExtension: "mp3") else {
+                print("can't find resource")
+                return
+        }
+            do {
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+                try AVAudioSession.sharedInstance().setActive(true)
+
+                objPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.m4a.rawValue)
+
+                guard let aPlayer = objPlayer else { return }
+                aPlayer.play()
+
+            } catch let error {
+                print(error.localizedDescription)
+            }
+    }
+    
+    @IBAction func howAreYouActionButton(_ sender: UIButton) {
+        
+        guard let url = Bundle.main.url(forResource: "HowAreYou", withExtension: "mp3") else {
+                print("can't find resource")
+                return
+        }
+            do {
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+                try AVAudioSession.sharedInstance().setActive(true)
+
+                objPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.m4a.rawValue)
+
+                guard let aPlayer = objPlayer else { return }
+                aPlayer.play()
+
+            } catch let error {
+                print(error.localizedDescription)
+            }
+    }
+    
+    
+    @IBAction func pardonActionButton(_ sender: UIButton) {
+        
+        guard let url = Bundle.main.url(forResource: "Pardon", withExtension: "mp3") else {
+                print("can't find resource")
+                return
+        }
+            do {
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+                try AVAudioSession.sharedInstance().setActive(true)
+
+                objPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.m4a.rawValue)
+
+                guard let aPlayer = objPlayer else { return }
+                aPlayer.play()
+
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        
+    }
+    
+    
+    
+    
 }
 
 
